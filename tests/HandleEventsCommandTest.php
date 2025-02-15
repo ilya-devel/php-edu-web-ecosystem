28a4289351ff57452efb042acb10c8e6f1df28a7
ilya.devel <ilya.devel@yandex.ru>
2025-02-16 00:59:50 +0500

    homework3 completed

---
+++ tests/HandleEventsCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class HandleEventsCommandTest extends TestCase {
+
+    /**
+     * @dataProvider eventDtoDataProvider
+     */
+    public function testShouldEventBeRanReceiveEventDtoAndReturnCorrectBool(array $event, bool $shouldEventBeRan):void {
+        $handleEventCommand = new \App\Commands\HandleEventsCommand(new \App\Application(dirname(__DIR__)));
+        $result = $handleEventCommand->shouldEventBeRan($event);
+        self::assertEquals($result, $shouldEventBeRan);
+    }
+
+    public static function eventDtoDataProvider(): array {
+        return [
+            [
+                [
+                    'minute' => (int)date("i"),
+                    'hour' => (int)date("H"),
+                    'day' => (int)date("d"),
+                    'month' => (int)date("m"),
+                    'day_of_week' => (int)date("w")
+                ],
+                true
+            ],
+            [
+                [
+                    'minute' => (int)date("i"),
+                    'hour' => (int)date("H"),
+                    'day' => (int)date("d"),
+                    'month' => (int)date("m"),
+                    'day_of_week' => null
+                ],
+                true
+            ]
+        ];
+    }
+}
